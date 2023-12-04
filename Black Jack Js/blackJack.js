6fe7ad21616a742c90aea570e102e4829f6bc373
TheDhavid <dhavidadeyeye@gmail.com>
2023-12-04 21:20:35 +0100

    Completion of blackjack game prototype
    Update on code to complete the Blackjack game project

--- Black Jack Js/blackJack.js
+++ Black Jack Js/blackJack.js
@@ -1,17 +1,36 @@
-let firstCard = 11
-let secondCard = 10
-let sum = firstCard + secondCard
+let cards = [] 
+let sum = 0
 let hasBlackjack = false
-let isAlive = true
+let isAlive = false
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+let player = {
+    name : "Per",
+    chips : 145
+}
+
+let playerEl = document.getElementById("player-el")
+playerEl.textContent = player.name + ": $" + player.chips
+
 
 function startGame() {
+    let firstCard = getRandomCard()
+    let secondCard = getRandomCard()
+    cards = [firstCard, secondCard] // array - ordered list of items
+    sum = firstCard + secondCard
+    isAlive = true
+    renderGame()
+}
+
+function renderGame() {
     sumEl.textContent = "Sum: " + sum
-    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    cardsEl.textContent = "Cards:" 
+    for (let i = 0; i < cards.length; i++) {
+        cardsEl.textContent += " " + cards[i]
+    }
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
     } else if(sum == 21) {
@@ -23,3 +42,26 @@ function startGame() {
     }
     messageEl.textContent  = message 
 }
+
+function newCard() {
+    if (isAlive === true && hasBlackjack === false) {
+        let card = getRandomCard()
+        sum += card
+        cards.push(card)
+        renderGame()
+    }
+}
+
+
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random() * 13) + 1
+    if (randomNumber === 1) {
+        return 11
+    } else if (randomNumber > 10) {
+        return 10
+    } else {
+        return randomNumber
+    }
+}
+
+
